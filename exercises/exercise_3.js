6ab08412aed787ca5c2b07739b5da703e48e88ea
juliasut <juliasutygina@gmail.com>
2022-09-27 15:25:57 -0600

    refactor[charactersWithMediumHeight]: Add funtions signature

--- exercises/exercise_3.js
+++ exercises/exercise_3.js
@@ -1,13 +1,12 @@
-const characters = require("../data/characters.json");
+const characters = require('../data/characters.json');
 
 // TODO: return only characters that
 // their height is between 180 and 200
 
 const charactersWithMediumHeight = () => {
-  // YOUR CODE HERE
-  // YOU CAN CHANGE ANY CODE INSIDE THE FUNCTION
   const MIN_HEIGHT = 0;
   const MAX_HEIGHT = 300;
+  return characters.filter(({height}) => height > 180 && height < 200);
 };
 
 module.exports = charactersWithMediumHeight;
