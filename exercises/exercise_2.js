57e2c5dbb6f4a5bfb107e956be05527b875063bb
juliasut <juliasutygina@gmail.com>
2022-09-27 12:56:11 -0600

    refactor: Add solution to charactersWithKnownBirthDate

--- exercises/exercise_2.js
+++ exercises/exercise_2.js
@@ -2,7 +2,11 @@ const characters = require("../data/characters.json");
 
 // TODO: return all characters with known birth year
 const charactersWithKnownBirthDate = () => {
-  // CODE HERE
+  const characterWithKnownBirthDate = characters.filter(
+    (char) => char.birth_year !== 'unknown'
+  );
+  return characterWithKnownBirthDate;
 };
 
+console.log(charactersWithKnownBirthDate())
 module.exports = charactersWithKnownBirthDate;
